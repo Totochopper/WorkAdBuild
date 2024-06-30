9fb9cc823b6816dd78633eeeef6896c46eb8b1b9
Totochopper <tony.descas@insidegroup.fr>
2024-06-30 17:13:37 +0200

    Create Special_Zones_Option.tsx

---
+++ Special_Zones_Option.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Special_Zones_Option" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="tilesets/WA_Special_Zones.png" width="192" height="64"/>
+</tileset>
