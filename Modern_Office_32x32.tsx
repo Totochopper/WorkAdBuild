aaa8f3b54463ac3b114a01523f311a932c50ffed
Totochopper <tony.descas@insidegroup.fr>
2024-06-30 17:13:30 +0200

    Create Modern_Office_32x32.tsx

---
+++ Modern_Office_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Modern_Office_32x32" tilewidth="32" tileheight="32" tilecount="848" columns="16">
+ <image source="tilesets/Modern_Office_32x32.png" width="512" height="1696"/>
+</tileset>
